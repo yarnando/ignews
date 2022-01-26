e3311b6e690e02cc293c6085e196736d6df68e70
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-01-26 15:44:46 -0300

    setup: prismic CMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 	);
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 	const price = await stripe.prices.retrieve('price_1KK5WaJmlEUK1uLQHkO4cJNr')
 
 	const product = { 
@@ -53,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 	return {
 		props: {
 			product
-		}
+		},
+		revalidate: 60 * 60 * 24, //24 hours
 	}
 }
