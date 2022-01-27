2b5852e095b53c7edfdec86e053545aa666b549d
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-01-27 18:57:50 -0300

    feat(posts): rendering posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,39 +3,40 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client'
+import { RichText } from "prismic-dom";
 import styles from './styles.module.scss'
 
-export default function Posts() {
+
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts } : PostsProps) {
     return (
         <>
-        <Head>
-            <title>Posts | Ignews</title>
-        </Head>
-        <main className={styles.container}>
-            <div className={styles.posts}>
-                <a href="">
-                    <time>
-                        12 de março de 2022
-                    </time>
-                    <strong>Creating a monorepo with lLerna & Yarn Workspaces</strong>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nesciunt delectus quaerat consectetur dolorum error cupiditate inventore architecto cumque voluptates, non aperiam neque accusamus asperiores voluptate molestias tempore dolorem magni.</p>
-                </a>
-                <a href="">
-                    <time>
-                        12 de março de 2022
-                    </time>
-                    <strong>Creating a monorepo with lLerna & Yarn Workspaces</strong>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nesciunt delectus quaerat consectetur dolorum error cupiditate inventore architecto cumque voluptates, non aperiam neque accusamus asperiores voluptate molestias tempore dolorem magni.</p>
-                </a>
-                <a href="">
-                    <time>
-                        12 de março de 2022
-                    </time>
-                    <strong>Creating a monorepo with lLerna & Yarn Workspaces</strong>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nesciunt delectus quaerat consectetur dolorum error cupiditate inventore architecto cumque voluptates, non aperiam neque accusamus asperiores voluptate molestias tempore dolorem magni.</p>
-                </a>
-            </div>
-        </main>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {posts.map(post => (
+                        <a href="#" key={post.slug}>
+                            <time>
+                                {post.updatedAt}
+                            </time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
+                </div>
+            </main>
         </>
     );
 }
@@ -43,18 +44,29 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const response = await prismic.query([
+    const response = await prismic.query<any>([
         Prismic.predicates.at('document.type', 'publication')
     ], {
         fetch: ['publication.title', 'publication.content'],
         pageSize: 100,
     })
 
-    console.log(JSON.stringify(response, null, 2));
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
 
     return {
         props: {
-
+            posts
         }
     }
 }
