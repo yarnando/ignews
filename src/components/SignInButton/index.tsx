5a3133653f669bd5dd387aefce2fb069100ee103
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-02-03 18:54:27 -0300

    feat: OAuth with GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,24 +2,32 @@ import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 export function SignInButton() {
 
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-    return  isUserLoggedIn ? (
+    console.log(session);
+
+    return session ? (
         <button 
             type="button"
-            className={styles.signInButton}>
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
             <FaGithub 
                 color="#04d361"
             />
-            Fernando Silva
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button 
             type="button"
-            className={styles.signInButton}>
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub 
                 color="#eba417"
             />
