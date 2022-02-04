6b79d11d786190c23d215ab5250b33f99c72f030
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-02-04 15:19:00 -0300

    setup: FaunaDB ok

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { query as q } from "faunadb"
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -13,6 +16,52 @@ export default NextAuth({
         }
       }
     }),
-    // ...add more providers here
   ],
+  // jwt: {
+  //   signingKey: process.env.SIGNIN_KEY
+  // },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            //se usuario não existir...
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            //crie um
+            q.Create(
+              q.Collection('users'),
+              {
+                data: {
+                  email
+                }
+              }
+            ),
+            //senao, apenas busque
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+        )
+
+        return true
+
+      } catch (error) {
+
+        return false
+
+      }
+    }
+  }
 })
