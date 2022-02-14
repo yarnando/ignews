c1bc987b7b37ceba6d452284be5e75ed03f2b33e
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-02-14 14:24:42 -0300

    feat(stripe/fauna): stripe webhooks+user creation

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,42 @@
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string,
+) {
+    // Buscar o usuario no banco do fauna com o ID customerId
+    // Salvar os dados da subscription do usuario no FaunaDB
+    const userRef = await fauna.query(
+        q.Select(
+            "ref", //pega só o ref do user
+            q.Get(
+                q.Match(
+                    q.Index('user_by_stripe_customer_id'),
+                    customerId
+                )
+            )
+        )
+    )
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId) //busca só 1 item
+
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id,
+
+    }
+
+    await fauna.query(
+        q.Create(
+            q.Collection('subscriptions'),
+            {
+                data: subscriptionData
+            }
+        )
+    )
+    
+}
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = []
@@ -26,8 +27,8 @@ const relevantEvents = new Set([
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    
-    if(req.method === 'POST') {
+
+    if (req.method === 'POST') {
         const buf = await buffer(req)
         const secret = req.headers['stripe-signature']
 
@@ -41,12 +42,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         const { type } = event
 
-        if(relevantEvents.has(type)) {
-            console.log('Evento recebido', event);
-            
+        if (relevantEvents.has(type)) {
+            try {
+                switch (type) {
+                    case 'checkout.session.completed':
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        )
+                        break;
+
+                    default:
+                        throw new Error('Unhandled event')
+                }
+            } catch (err) {
+                // nao pode retornar status de erro porque o stripe vai ficar tentando pra sempre
+                return res.json({ error: 'Webhook handler failed' })
+            }
         }
 
-        res.json({ received:true }) 
+        res.json({ received: true })
     } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
