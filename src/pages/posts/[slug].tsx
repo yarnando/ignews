f1737a18b8b70a40398d41113cb7d3e1595c717d
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-02-15 14:00:10 -0300

    feat(subscription button): validating subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,13 +40,15 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
     const session = await getSession({ req })
     const { slug } = params
-
-    console.log(session);
     
-
-    // if(!session) {
-
-    // }
+    if(!session.activeSubscription) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
 
     const prismic = getPrismicClient(req)
 
