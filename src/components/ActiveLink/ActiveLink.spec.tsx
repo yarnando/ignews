2339d3e8255bcb4ee7375f9164b77711376ec6fd
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-04-19 16:58:28 -0300

    test: first tests

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,48 @@
+import { render } from '@testing-library/react'
+import { ActiveLink } from '.'
+
+jest.mock('next/router', () => {
+    return {
+        useRouter() {
+            return {
+                asPath: '/'
+            }
+        }
+    }
+})
+
+describe('ActiveLink component', () => {
+
+    it('renders correctly', () => {
+        const { getByText } = render(
+            <ActiveLink
+                href="/"
+                activeClassName="active"
+            >
+                <a>Home</a>
+            </ActiveLink>
+        )
+    
+        //se quiser dar um console.log do que foi renderizado
+        // debug()
+    
+        expect(getByText('Home')).toBeInTheDocument()
+    })
+    
+    it('adds active class if the link is currently active', () => {
+        const { getByText } = render(
+            <ActiveLink
+                href="/"
+                activeClassName="active"
+            >
+                <a>Home</a>
+            </ActiveLink>
+        )
+    
+        //se quiser dar um console.log do que foi renderizado
+        // debug()
+    
+        expect(getByText('Home')).toHaveClass('active')
+    })
+
+})
