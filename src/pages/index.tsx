6200b8d2a5c99a49be0ea9594dbb68684316a6cb
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-04-26 14:23:36 -0300

    test: home page tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import styles from './home.module.scss'
 interface HomeProps {
 	product: {
 		priceId: string;
-		amount: number;
+		amount: string;
 	}
 }
 
@@ -31,7 +31,7 @@ export default function Home({ product }: HomeProps) {
 						<br />
 						<span>for {product.amount} month</span>
 					</p>
-					<SubscribeButton priceId={product.priceId} />
+					<SubscribeButton />
 				</section>
 				<img src="/images/avatar.svg" alt="Girl coding" />
 			</main>
---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
+import Home, { getStaticProps } from '../../pages'
+import { stripe } from '../../services/stripe'
+
+jest.mock('next/router')
+jest.mock('next-auth/client', () => {
+    return {
+        useSession: () => ({data: null, status: "unauthenticated"})
+    }
+})
+jest.mock('../../services/stripe')
+
+describe('Home page', () => {
+
+    it('renders correctly', () => {
+
+        render(<Home
+            product={{ priceId: 'fake-price-id', amount: 'R$10,00' }}
+        />)
+
+        expect(screen.getByText("for R$10,00 month")).toBeInTheDocument()
+        //exp. regular
+    })
+
+    it('loads initial data', async () => {
+        const retrieveStripePricesMocked = mocked(stripe.prices.retrieve)
+
+        retrieveStripePricesMocked.mockResolvedValueOnce({
+            id: 'fake-price-id',
+            unit_amount: 1000,
+        } as any)
+
+        const response = await getStaticProps({})
+
+        expect(response).toEqual( // se fosse só isso, verificaria se é estritamente igual
+            expect.objectContaining({ // como tem isso, verifica se tem pelo menos essas informações
+                props: {
+                    product: {
+                        priceId: 'fake-price-id',
+                        amount: '$10.00'
+                    }
+                }
+            })
+        )
+
+    })
+})
